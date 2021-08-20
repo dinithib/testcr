740b76cae9d7ac5ee71f7e93b02994b8d08f4c12
dinithib <mailtodinithi@gmail.com>
2021-08-20 10:44:52 +1000

    test123

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //test
+        //test 123
         setContentView(R.layout.activity_main)
     }
 
